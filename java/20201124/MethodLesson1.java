5c616ed765df36ad3e823744ff5f8fd38795978a
sakuma17 <ids08663@gmail.com>
2020-11-24 10:07:12 +0900

    オーバーロード

---
+++ java/20201124/MethodLesson1.java
@@ -0,0 +1,25 @@
+public class MethodLesson1{
+	public static void main(String[]args){
+
+	}
+	public static int add(int x,int y){
+		return x+y;
+	}
+	public static double add(double x,double y){
+		return x+y;
+	}
+	public static String add(String x,String y){
+		return x+y;
+	}
+	public static int add(int x,int y,int z){
+		return x+y+z;
+	}
+
+	/*public static int add(int a,int b){
+		return a+b;
+	ki}引数の組み合わせが同じなのでNG*/
+
+	/*public void int add(int a,int b){
+		return a+b;
+	ki}戻り値が違うとしてもNG*/
+}
