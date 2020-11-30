45c4dec50b60bc10b1ddfd78e6a615eab049d2bd
sakuma17 <ids08663@gmail.com>
2020-11-30 21:26:42 +0900

    SI.java修正

--- java/20201130/SI.java
+++ java/20201130/SI.java
@@ -1,5 +1,48 @@
 import java.util.*;
 public class SI{
+	public static void main(String[]args){
+		String[] params={"体力","魔力","パワー","きようさ","すばやさ"};
+		String[] races={"人間","ハイエルフ","トロル","ノーム"};
+		String[] classes={"戦士","盗賊","僧侶","魔術師"};
+		int[][] raceMatrix={
+			{10,10,10,10,10},
+			{0,20,0,10,20,},
+			{30,0,20,0,0},
+			{10,0,0,25,20},
+		};
+		double[][] classMatrix={
+			{1.6,1,1.4,1,1},
+			{1.1,1,1.2,1.3,1.3},
+			{1.3,1.5,1.1,1,1},
+			{1,1.9,1,1,1.1},
+		};
+
+		Scanner sc=new Scanner(System.in);
+		System.out.print("名前を入力してください>");
+		String name=sc.nextLine();
+		int seed=calcSeed(name);
+		int[] status=makeStatus(seed,new int[]{100,100,50,50,50});
+		System.out.println("初期ステータスが決定しました");
+		showStatus(status,params);
+		System.out.print("種族を選んでください 0...人間,1...ハイエルフ,2...トロル,3...ノーム>");
+		int race=sc.nextInt();
+		System.out.println(races[race]+"のボーナスが適用されました!");
+		raceBonus(status,raceMatrix[race]);
+		showStatus(status,params);
+		System.out.print("職業を選んでください 0...戦士,1...盗賊,2...僧侶,3...魔術師>");
+		int cls=sc.nextInt();
+		System.out.println(classes[cls]+"のボーナスが適用されました!");
+		classBonus(status,classMatrix[cls]);
+		showStatus(status,params);
+		System.out.println("***作成成功！***");
+		System.out.printf("私は%sの%s、%sです。%n",classes[cls],races[race],name);
+		System.out.println("能力値("+sumStatus(status)+")を高い順に並べると");
+		sortStatus(status,params);
+		for(int i=0;i<status.length;i++){
+			System.out.printf("%s:%d%n",params[i],status[i]);
+		}
+		System.out.println("です。今後ともよろしく...。");
+	}//mainの終わり
 	static int calcSeed(String name){
 		int seed=0;
 		for(int i=0;i<name.length();i++){
@@ -36,7 +79,7 @@ public class SI{
 		for(int i=0;i<status.length-1;i++){
 			for(int j=i+1;j<status.length;j++){
 				if(status[i]<status[j]){
-					int t1=status[j];
+					int t1=status[i];
 					status[i]=status[j];
 					status[j]=t1;
 					String t2=params[i];
@@ -49,50 +92,8 @@ public class SI{
 	static void showStatus(int[] status,String[] params){
 		String str="";
 		for(int i=0;i<status.length;i++){
-			str+=params[i]+":"+status[i]+" ";
+			str += params[i] + ":" + status[i] +" ";
 		}
 		System.out.println("["+str+"]");
-	}
-	public static void main(String[]args){
-		String[] params={"体力","魔力","パワー","きようさ","すばやさ"};
-		String[] races={"人間","ハイエルフ","トロル","ノーム"};
-		String[] classes={"戦士","盗賊","僧侶","魔術師"};
-		int[][] raceMatrix={
-			{10,10,10,10,10},
-			{0,20,0,10,20},
-			{30,0,20,0,0},
-			{10,0,0,25,20},
-		};
-		double[][] classMatrix={
-			{1.6,1,1.4,1,1},
-			{1.1,1,1.2,1.3,1.3},
-			{1.3,1.5,1.1,1,1},
-			{1,1.9,1,1,1.1},
-		};
-	}
-	Scanner sc=new Scanner(System.in);
-	System.out.print("名前を入力してください");
-	String name=sc.nextLine();
-	int seed=calcSeed(name);
-	int[] status=makeStatus(seed,new int[]{100,100,50,50,50});
-	System.out.println("初期ステータスが決定しました");
-	showStatus(status,params);
-	System.out.print("種族を選んでください 0...人間,1...ハイエルフ,2...トロル,3...ノーム>");
-	int race=sc.nextInt();
-	System.out.println(races[race]+"のボーナスが適用されました");
-	raceBonus(status,params);
-	System.out.print("職業を選んでください 0...戦士,1...盗賊,2...僧侶,3...魔術師>");
-	int cls=sc.nextInt();
-	System.out.println(classes[cls]+"のボーナスが適用されました");
-	classBonus(status,classMatrix[cls]);
-	showStatus(status,params);
-	System.out.println("***作戦成功!***");
-	System.out.printf("私は%sの%s、%sです。%n",classes[cls],races[race],name);
-	System.out.println("能力値("+sumStatus(status)+")を高い順に並べると");
-	sortStatus(status,params);
-	for(int i=0;i<status.length;i++){
-		System.out.printf("%s:%d%n",params[i],status[i])
-	}
-	System.out.println("です。今後ともよろしく…。");
 	}
 }
