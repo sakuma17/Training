32eee40cbc8f3ce072b196469c416d8409cbbe81
sakuma17 <ids08663@gmail.com>
2020-12-03 18:24:12 +0900

    削除

--- java/1203/SplitLesson.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class SplitLesson{
-	public static void main(String[]args){
-		String str="りんご,みかん,ばなな";//CSVデータ:カンマ区切りデータ
-		String[] data=str.split(",");
-		System.out.println(Arrays.toString(data));
-	}
-}
