982d35cea264fad3e2caa5c40f17e9d4bc47ffe5
sakuma17 <ids08663@gmail.com>
2020-12-15 09:33:03 +0900

    update

--- java/20201211/MapTest3.java
+++ java/20201211/MapTest3.java
@@ -11,16 +11,22 @@ public class MapTest3{
 				//2行をまとめると
 				//map.put(num,map.get(num)+1);
 			}else{
-				map.put(i,1);
+				map.put(num,1);
 			}
 		}
 		System.out.println("1~100の乱数を100回発生させたよ");
 		System.out.println("***result***");
 		System.out.printf("%d種類の数値が出ました。%n",map.size());
+		for(int key:map.keySet()){
+			System.out.printf("%d…%d回%n",key,map.get(key));
+		}
+		/*
+		System.out.println();
 		for(int i=1;i<=100;i++){
 			if(map.containsKey(i)){
 				System.out.printf("%d…%d回%n",i,map.get(i));
 			}
 		}
+		*/
 	}
 }
